86e7c3fc7ccfd0cd683d218b421cab261723e2a3
Acatera <dersummoner@gmail.com>
2024-09-04 07:52:23 +0300

    Added main components

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+	import AreaInfo from "./components/AreaInfo.svelte";
+	import CharacterInfo from "./components/CharacterInfo.svelte";
+	import Combat from "./components/Combat.svelte";
+	import Controls from "./components/Controls.svelte";
+	import EventLog from "./components/EventLog.svelte";
 </script>
 
 <main>
+	<CharacterInfo />
+	<AreaInfo />
+	<Combat />
+	<Controls />
+	<EventLog />
 </main>
 
 <style>
-
 </style>
