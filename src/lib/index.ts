1eca4fd9fee4d5a4fc1ea7a93ed3bf7764c43721
Acatera <dersummoner@gmail.com>
2024-09-04 07:20:59 +0300

    Init Svelte app. Move code from old version. Not working

---
+++ src/lib/index.ts
@@ -0,0 +1,23 @@
+// import Game from './models/game';
+
+// const game = new Game();
+// await game.start();
+
+// // Add event listener for keydown events
+// document.addEventListener('keydown', (event) => {
+//   if (event.key === 'a') {
+//     game.attack();
+//   }
+
+//   if (event.key === 'i') {
+//     game.toggleInventory();
+//   }
+// });
+
+// const attackControl = document.getElementById('attack-control');
+
+// if (attackControl) {
+//   attackControl.addEventListener('click', function() {
+//     game.attack();
+//   });
+// }
