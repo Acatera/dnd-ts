928e5c0650db18b55383e46cb2b5efd163c49a72
Acatera <dersummoner@gmail.com>
2024-09-06 15:29:14 +0300

    Revert rollup

--- rollup.config.js
+++ rollup.config.js
@@ -43,33 +43,42 @@ export default {
 		svelte({
 			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: {
-				dev: !production // enable run-time checks when not in production
+				// enable run-time checks when not in production
+				dev: !production
 			}
 		}),
-		css({ output: 'bundle.css' }), // extract component CSS into a separate file
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({ output: 'bundle.css' }),
 
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
 			dedupe: ['svelte'],
 			exportConditions: ['svelte']
 		}),
 		commonjs(),
-
 		typescript({
 			sourceMap: !production,
-			inlineSources: !production,
-			noEmitOnError: true // Stop build on TypeScript errors
+			inlineSources: !production
 		}),
 
-		!production && serve(), // In dev mode, call `npm run start` once the bundle has been generated
+		// In dev mode, call `npm run start` once
+		// the bundle has been generated
+		!production && serve(),
 
-		!production && livereload('public'), // Watch the `public` directory and refresh the browser on changes when not in production
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('public'),
 
-		production && terser() // Minify for production builds
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
 	],
-	onwarn: () => {
-		// Ignore all warnings
-	},
 	watch: {
 		clearScreen: false
 	}
